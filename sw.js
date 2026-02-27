181e38d0989e1377faaffa448b6f5ba281c16eec
AlexanderClark11 <alexanderclark.ail@gmail.com>
2026-02-27 18:19:46 -0500

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,62 @@
+const CACHE_NAME = "li-practice-cache-v1";
+
+const FILES_TO_CACHE = [
+  "/",
+  "/index.html",
+  "/multiple-choice.html",
+  "/MIexam.js",
+  "/manifest.webmanifest",
+  "/icon-192.png",
+  "/icon-512.png"
+];
+
+// INSTALL
+self.addEventListener("install", event => {
+
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(FILES_TO_CACHE);
+    })
+  );
+
+  self.skipWaiting();
+
+});
+
+
+// ACTIVATE (delete old cache)
+self.addEventListener("activate", event => {
+
+  event.waitUntil(
+    caches.keys().then(keys => {
+
+      return Promise.all(
+        keys.map(key => {
+
+          if (key !== CACHE_NAME) {
+            return caches.delete(key);
+          }
+
+        })
+      );
+
+    })
+  );
+
+  self.clients.claim();
+
+});
+
+
+// FETCH
+self.addEventListener("fetch", event => {
+
+  event.respondWith(
+    caches.match(event.request).then(response => {
+
+      return response || fetch(event.request);
+
+    })
+  );
+
+});
